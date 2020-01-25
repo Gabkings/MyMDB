3d1afcb9a6244dad13a230de4d1bbf4792f9bf99
developer <developer@localhost.localdomain>
2020-01-25 23:55:20 +0300

    added voting feature to movies

--- django/core/models.py
+++ django/core/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.aggregates import (
+    Sum
+)
+from django.conf import settings
 
 # Create your models here.
 
@@ -10,6 +14,11 @@ class MovieManager(models.Manager):
             'writers','actors'
         )
         return qs 
+
+    def all_with_related_persons_and_score(self):
+        qs = self.all_with_related_persons()
+        qs = qs.annotate(score=Sum('vote__value'))
+        return qs
 
 class Movie(models.Model):
 
@@ -29,7 +38,7 @@ class Movie(models.Model):
     year = models.PositiveIntegerField()
     rating = models.IntegerField(
         choices=RATINGS,
-        default=NOT_RATED
+        default=NOT_RATED,
     )
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank=True)
@@ -112,3 +121,45 @@ class Role(models.Model):
     class Meta:
         unique_together = ('movie','person','name')
     
+class VoteManager(models.Manager):
+
+    def get_vote_or_unsaved_blank_vote(self, movie, user):
+        try:
+            return Vote.objects.get(
+                movie=movie,
+                user=user)
+        except Vote.DoesNotExist:
+            return Vote(
+                movie=movie,
+                user=user)
+
+
+
+
+class Vote(models.Model):
+    UP = 1
+    DOWN = -1
+    VALUE_CHOICES = (
+        (UP, "👍",),
+        (DOWN, "👎",),
+    )
+
+    value = models.SmallIntegerField(
+        choices=VALUE_CHOICES,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+    )
+    voted_on = models.DateTimeField(
+        auto_now=True
+    )
+
+    objects = VoteManager()
+
+    class Meta:
+        unique_together = ('user', 'movie')    
